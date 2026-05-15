7712b97ed9ed68318863a715bd841fd9fa4f378a
SankalpThorat02 <sankalpt2225@gmail.com>
2026-05-15 14:37:27 +0530

    feat: setup auth service

---
+++ src/main/java/com/sankalp/recruito/service/AuthService.java
@@ -0,0 +1,22 @@
+package com.sankalp.recruito.service;
+
+import com.sankalp.recruito.config.SecurityConfig;
+import com.sankalp.recruito.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
+}
