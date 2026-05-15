2b772de90475eafb00351bc4a357eac3d7f1dd1a
SankalpThorat02 <sankalpt2225@gmail.com>
2026-05-15 10:32:52 +0530

    Initial commit

---
+++ src/test/java/com/sankalp/recruito/RecruitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sankalp.recruito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
